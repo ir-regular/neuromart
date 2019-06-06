9a8113b5de3cecf6c64f9a3009be15383fc2a395
Jane Olszewska <3jane@3jane.co.uk>
2019-06-06 13:54:43 +0100

    Dev: pct variance explained table now uses dbc

--- neuromart/tables.py
+++ neuromart/tables.py
@@ -1,28 +1,28 @@
 # -*- coding: utf-8 -*-
-from dash_table import DataTable
+import dash_html_components as html
+import dash_bootstrap_components as dbc
+import pandas as pd
 
 
 def pls_component_list(var_x, var_y):
-    data = [{'rank': i + 1,
-             'pls-comp': "PLS" + str(i + 1),
-             'var-x': var_x[i],
-             'var-y': var_y[i],
-             # TODO: that should be pval from perm test
-             # 'p-val': "TODO"
-             }
-            for i in range(0, 5)]
+    rank = list(range(1, 6))
+    pls_component = ["PLS" + str(i) for i in rank]
 
-    columns = {'rank': 'Rank',
-               'pls-comp': 'PLS comp',
-               'var-x': '% variance expl in X',
-               'var-y': '% variance expl in Y',
-               # 'p-val': 'P-value for PLS'
-               }
+    df = pd.DataFrame({
+        "Rank": rank,
+        "PLS component": pls_component,
+        "% variance expl in X": var_x[:5],
+        "% variance expl in Y": var_y[:5],
+        # TODO: "P-value for PLS"
+    })
 
-    columns = [{"id": col_id, "name": name} for col_id, name in columns.items()]
+    table = dbc.Table.from_dataframe(df, striped=True)
+    body = table.children[1]
+    body.children.append(html.Tr([
+        html.Td(colSpan=2),
+        html.Td(dbc.Button("Download VarX.csv", color="secondary")),
+        html.Td(dbc.Button("Download VarY.csv", color="secondary")),
+        # TODO: html.Td(dbc.Button("Download Pval", color="secondary")),
+    ]))
+    return table
 
-    return DataTable(
-        id='table',
-        columns=columns,
-        data=data,
-    )
