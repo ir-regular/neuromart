e4df33109cf79e84a153b7cb79ea297bfb792307
Jane Olszewska <3jane@3jane.co.uk>
2019-06-06 11:45:53 +0100

    Dev: removed a column from summary table; misunderstood description

--- neuromart/tables.py
+++ neuromart/tables.py
@@ -2,19 +2,22 @@
 from dash_table import DataTable
 
 
-def pls_component_list(var_x, var_y, p):
+def pls_component_list(var_x, var_y):
     data = [{'rank': i + 1,
              'pls-comp': "PLS" + str(i + 1),
              'var-x': var_x[i],
              'var-y': var_y[i],
-             'p-val': str(p[i, 0]) + " " + str(p[i, 1])}  # TODO: probably wrong value; confirm
+             # TODO: that should be pval from perm test
+             # 'p-val': "TODO"
+             }
             for i in range(0, 5)]
 
     columns = {'rank': 'Rank',
                'pls-comp': 'PLS comp',
                'var-x': '% variance expl in X',
                'var-y': '% variance expl in Y',
-               'p-val': 'P-value for PLS'}
+               # 'p-val': 'P-value for PLS'
+               }
 
     columns = [{"id": col_id, "name": name} for col_id, name in columns.items()]
 
