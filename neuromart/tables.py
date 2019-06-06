a7fc93f0d42ab8f0c9af4ee1c259abcedba4ddd2
Jane Olszewska <3jane@3jane.co.uk>
2019-06-06 14:13:31 +0100

    Dev: value formatting in preview tables, precision 2

--- neuromart/tables.py
+++ neuromart/tables.py
@@ -7,12 +7,14 @@ import pandas as pd
 def pls_component_list(var_x, var_y):
     rank = list(range(1, 6))
     pls_component = ["PLS" + str(i) for i in rank]
+    var_x_formatted = ['{:.2f}'.format(var_x[i]) for i in range(5)]
+    var_y_formatted = ['{:.2f}'.format(var_y[i]) for i in range(5)]
 
     df = pd.DataFrame({
         "Rank": rank,
         "PLS component": pls_component,
-        "% variance expl in X": var_x[:5],
-        "% variance expl in Y": var_y[:5],
+        "% variance expl in X": var_x_formatted,
+        "% variance expl in Y": var_y_formatted,
         # TODO: "P-value for PLS"
     })
 
@@ -28,8 +30,9 @@ def pls_component_list(var_x, var_y):
 
 
 def pls_r_p(r, p):
-    r_column = [r[i, 1] for i in range(0, 5)]
-    p_column = [p[i, 1] for i in range(0, 5)]
+    # TODO: both r and p have two columns, is it correct that I'm selecting the second column?
+    r_column = ['{:.2f}'.format(r[i, 1]) for i in range(0, 5)]
+    p_column = ['{:.2f}'.format(p[i, 1]) for i in range(0, 5)]
     df = pd.DataFrame({
         "PLS component": ["PLS" + str(i) for i in range(1, 6)],
         "R": r_column,
